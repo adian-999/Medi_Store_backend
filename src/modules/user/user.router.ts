d5f1687f1f791a7a9b5fb76ee17ac697377def84
adian-999 <adianahmmed999@gmail.com>
2026-03-10 01:52:54 +0600

    user delete

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
 import { userController } from "./user.controller";
+import middleAuth, { UserRole } from "../../middleware/authMiddle";
 
 const router = express.Router();
 
-router.delete("/:id",userController.deleteUser)
+router.delete("/:id",middleAuth(UserRole.ADMIN) as any,userController.deleteUser)
 
 export const userRouter:Router = router;
